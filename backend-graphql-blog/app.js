10dfb0c801ce6c09a2ffade3e718e85694aec38f
Mark <ratrateroo@gmail.com>
2021-04-15 23:31:39 +0800

    started configuring graphql middleware

--- backend-graphql-blog/app.js
+++ backend-graphql-blog/app.js
@@ -14,8 +14,10 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/', (req, res, next) => {
-	res.send('Hello World!');
-});
-
-app.listen(3000);
+app.use(
+	'/graphql',
+	graphqlHTTP({
+		schema: graphQlSchema,
+		graphiql: true,
+	})
+);
