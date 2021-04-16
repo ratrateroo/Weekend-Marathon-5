973d642fa5d71c70c6412b5c5f9b344a2d8b81f4
Mark <ratrateroo@gmail.com>
2021-04-16 23:51:04 +0800

    added user model for mongodb

--- backend-graphql-blog/graphql/resolvers/index.js
+++ backend-graphql-blog/graphql/resolvers/index.js
@@ -1,1 +1,3 @@
 const bcrypt = require('bcryptjs');
+
+const User = require('../../models/user');
---
+++ backend-graphql-blog/models/user.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+	username: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	firstname: {
+		type: String,
+		required: true,
+	},
+	middlename: {
+		type: String,
+		required: true,
+	},
+	lastname: {
+		type: String,
+		required: true,
+	},
+
+	blogs: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: 'Blog',
+		},
+	],
+	frieds: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: 'User',
+		},
+	],
+});
+
+module.exports = mongoose.model('User', userSchema);
