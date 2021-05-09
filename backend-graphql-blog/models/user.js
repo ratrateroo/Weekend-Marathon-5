d55df0435760d1e1b4279e28ba354bb715cf0ef7
Mark <ratrateroo@gmail.com>
2021-05-09 21:39:14 +0800

    updated user model

--- backend-graphql-blog/models/user.js
+++ backend-graphql-blog/models/user.js
@@ -2,44 +2,47 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-	username: {
-		type: String,
-		required: true,
-	},
-	email: {
-		type: String,
-		required: true,
-	},
-	password: {
-		type: String,
-		required: true,
-	},
-	firstname: {
-		type: String,
-		required: true,
-	},
-	middlename: {
-		type: String,
-		required: true,
-	},
-	lastname: {
-		type: String,
-		required: true,
-	},
+const userSchema = new Schema(
+	{
+		username: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		firstname: {
+			type: String,
+			required: true,
+		},
+		middlename: {
+			type: String,
+			required: true,
+		},
+		lastname: {
+			type: String,
+			required: true,
+		},
 
-	createdBlogs: [
-		{
-			type: Schema.Types.ObjectId,
-			ref: 'Blog',
-		},
-	],
-	friends: [
-		{
-			type: Schema.Types.ObjectId,
-			ref: 'User',
-		},
-	],
-});
+		createdBlogs: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: 'Blog',
+			},
+		],
+		friends: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: 'User',
+			},
+		],
+	},
+	{ timestamps: true }
+);
 
 module.exports = mongoose.model('User', userSchema);
