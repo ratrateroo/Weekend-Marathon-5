d84304d354a4221937b214cdd319e3a0fee49d8b
Mark <ratrateroo@gmail.com>
2021-05-11 22:31:33 +0800

    updated friends model schema

--- backend-graphql-blog/models/friends.js
+++ backend-graphql-blog/models/friends.js
@@ -2,15 +2,18 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const friendsSchema = new Schema({
-	friend: {
-		type: Schema.Types.ObjectId,
-		ref: 'User',
+const friendsSchema = new Schema(
+	{
+		friend: {
+			type: Schema.Types.ObjectId,
+			ref: 'User',
+		},
+		user: {
+			type: Schema.Types.ObjectId,
+			ref: 'User',
+		},
 	},
-	user: {
-		type: Schema.Types.ObjectId,
-		ref: 'User',
-	},
-});
+	{ timestamps: true }
+);
 
 module.exports = mongoose.model('Friend', friendsSchema);
