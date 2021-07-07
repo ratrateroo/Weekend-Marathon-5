c360c51af8f0048bbd1151b22e9e4cfa3c4a0f54
Mark <ratrateroo@gmail.com>
2021-07-07 23:04:08 +0800

    added default nav links

---
+++ frontend-graphql-blog/src/shared/components/Navigation/DefaultNavLinks.js
@@ -0,0 +1,32 @@
+import React, { useContext } from 'react';
+import { NavLink } from 'react-router-dom';
+
+import { AuthContext } from '../../context/auth-context';
+import './DefaultNavLinks.css';
+
+const NavLinks = (props) => {
+	const auth = useContext(AuthContext);
+
+	return (
+		<ul className="c-default-navigation__items">
+			<li className="c-default-navigation__item">
+				{/* <a href="../Blogs/Blogs.html" className="c-default-navigation__link">
+               Blogs
+            </a> */}
+				<NavLink to="/" className="c-default-navigation__link" exact>
+					Blogs
+				</NavLink>
+			</li>
+			<li className="c-default-navigation__">
+				{/* <a href="../User List/User List.html" className="c-default-navigation__link">
+               Users
+            </a> */}
+				<NavLink to="/" className="c-default-navigation__link" exact>
+					Users
+				</NavLink>
+			</li>
+		</ul>
+	);
+};
+
+export default NavLinks;
