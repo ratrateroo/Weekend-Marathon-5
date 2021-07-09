ec9e328749b93000a917e54e87cad67d2ff8151c
Mark <ratrateroo@gmail.com>
2021-07-09 22:31:02 +0800

    added main navigation component to the app

--- frontend-graphql-blog/src/App.js
+++ frontend-graphql-blog/src/App.js
@@ -1,15 +1,12 @@
 import './App.css';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import BlogsPage from './blogs/pages/blogs';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
 	return (
 		<BrowserRouter>
-			<Switch>
-				<Redirect from="/" to="/blogs" exact />
-
-				<Route path="/blogs" component={BlogsPage} />
-			</Switch>
+			<MainNavigation />
 		</BrowserRouter>
 	);
 };
