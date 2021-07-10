c0e3309bb0731d3935c9dda1e290cde5bf9d16ba
Mark <ratrateroo@gmail.com>
2021-07-10 23:36:41 +0800

    added auth context to the app

--- frontend-graphql-blog/src/App.js
+++ frontend-graphql-blog/src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
-import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import {
+	BrowserRouter as Router,
+	Route,
+	Redirect,
+	Switch,
+} from 'react-router-dom';
 
 import MainNavigation from './shared/components/Navigation/MainNavigation';
+import { AuthContext } from './shared/context/auth-context';
 
 const App = () => {
 	return (
-		<BrowserRouter>
-			<MainNavigation />
-		</BrowserRouter>
+		<AuthContext.Provider>
+			<Router>
+				<MainNavigation />
+			</Router>
+		</AuthContext.Provider>
 	);
 };
 
