c7ac1763e14c09e346e7650c18b370f325545249
Mark <ratrateroo@gmail.com>
2021-07-10 23:32:12 +0800

    added auth context

--- frontend-graphql-blog/src/App.js
+++ frontend-graphql-blog/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
-import BlogsPage from './blogs/pages/blogs';
+
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
