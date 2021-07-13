fe728d0a13c8b3d4879b3bc2b79f1229cdc8ae99
Mark <ratrateroo@gmail.com>
2021-07-13 23:04:09 +0800

    added backdrop and modal components

---
+++ frontend-graphql-blog/src/shared/components/UIElements/Modal.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { CSSTransition } from 'react-transition-group';
+
+import Backdrop from './Backdrop';
+import './Modal.css';
+
+const ModalOverlay = (props) => {
+	const content = (
+		<div className={`c-modal`} style={props.style}>
+			<header className={`c-modal__header`}>
+				<h2 className={`c-modal__title`}>{props.header}</h2>
+			</header>
+			<form
+				onSubmit={
+					props.onSubmit
+						? props.onSubmit
+						: (event) => event.preventDefault()
+				}>
+				<div className={`c-modal__content-`}>{props.children}</div>
+				<footer className={`c-modal__footer`}>{props.footer}</footer>
+			</form>
+		</div>
+	);
+
+	return ReactDOM.createPortal(content, document.getElementById('modal-hook'));
+};
+
+const Modal = (props) => {
+	return (
+		<React.Fragment>
+			{props.show && <Backdrop onClick={props.onCancel} />}
+			<CSSTransition
+				in={props.show}
+				mountOnEnter
+				unmountOnExit
+				timeout={200}
+				clasNames="c-modal">
+				<ModalOverlay {...props} />
+			</CSSTransition>
+		</React.Fragment>
+	);
+};
+
+export default Modal;
