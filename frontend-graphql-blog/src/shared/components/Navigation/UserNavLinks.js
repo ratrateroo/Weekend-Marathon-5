2702264d8903974a95ca05580a28612219a44d39
Mark <ratrateroo@gmail.com>
2021-07-20 00:00:34 +0800

    used auth context consumer on the user nav links

--- frontend-graphql-blog/src/shared/components/Navigation/UserNavLinks.js
+++ frontend-graphql-blog/src/shared/components/Navigation/UserNavLinks.js
@@ -5,49 +5,47 @@ import { AuthContext } from '../../context/auth-context';
 import './UserNavLinks.css';
 
 const NavLinks = (props) => {
-	const auth = useContext(AuthContext);
-
 	return (
-		<ul className="c-user-navigation__items">
-			{!auth.isLoggedIn && (
-				<React.Fragment>
-					<li className="c-user-navigation__item">
-						{/* <a href="../Signup/Signup.html" className="c-user-navigation__link">
-               Signup
-            </a> */}
-						<NavLink
-							to="/signup"
-							className="c-user-navigation__link"
-							exact>
-							Signup
-						</NavLink>
-					</li>
-					<li className="c-user-navigation__item">
-						{/* <a href="../Login/Login.html" className="c-user-navigation__link">
+		<AuthContext.Consumer>
+			{(context) => {
+				return (
+					<ul className="c-user-navigation__items">
+						{!context.isLoggedIn && (
+							<React.Fragment>
+								<li className="c-user-navigation__item">
+									<NavLink
+										to="/signup"
+										className="c-user-navigation__link"
+										exact>
+										Signup
+									</NavLink>
+								</li>
+								<li className="c-user-navigation__item">
+									{/* <a href="../Login/Login.html" className="c-user-navigation__link">
                Login
             </a> */}
-						<NavLink
-							to="/login"
-							className="c-user-navigation__link"
-							exact>
-							Login
-						</NavLink>
-					</li>
-				</React.Fragment>
-			)}
-			{auth.isLoggedIn && (
-				<li className="c--navigation__item">
-					{/* <a href="../Blogs/Blogs.html" className="c-user-navigation__link">
-               Logout
-            </a> */}
-					<button
-						className="c-user-navigation__link"
-						onClick={auth.logout}>
-						Logout
-					</button>
-				</li>
-			)}
-		</ul>
+									<NavLink
+										to="/login"
+										className="c-user-navigation__link"
+										exact>
+										Login
+									</NavLink>
+								</li>
+							</React.Fragment>
+						)}
+						{context.isLoggedIn && (
+							<li className="c--navigation__item">
+								<button
+									className="c-user-navigation__link"
+									onClick={context.logout}>
+									Logout
+								</button>
+							</li>
+						)}
+					</ul>
+				);
+			}}
+		</AuthContext.Consumer>
 	);
 };
 
