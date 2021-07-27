d171d737e6f5ee0cde27f1d10d74b9c49108df78
Mark <ratrateroo@gmail.com>
2021-07-27 22:54:42 +0800

    added blog item component

---
+++ frontend-graphql-blog/src/blogs/components/BlogItem.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import './BlogItem.css';
+
+const BlogItem = (props) => {
+	return (
+		<li className="o-blog-list__item">
+			<div className="c-post">
+				<div className="c-post__image">
+					<img
+						className="c-post__image-pic"
+						src={props.image}
+						alt={props.title}
+					/>
+				</div>
+
+				<div class="c-post__details">
+					<h2 className="c-post__title">{props.title}</h2>
+					<h3 className="c-post__author">{props.author}</h3>
+				</div>
+			</div>
+		</li>
+	);
+};
+
+export default BlogItem;
