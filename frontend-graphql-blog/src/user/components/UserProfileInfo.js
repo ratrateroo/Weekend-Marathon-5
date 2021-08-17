148d10e11bc482cdd44ec35e5ed1765eb829c5f5
Mark <ratrateroo@gmail.com>
2021-08-17 23:57:40 +0800

    added form to the update profile component

--- frontend-graphql-blog/src/user/components/UserProfileInfo.js
+++ frontend-graphql-blog/src/user/components/UserProfileInfo.js
@@ -3,7 +3,13 @@ import React, { useState } from 'react';
 import './UserProfileInfo.css';
 import dummy_image from '../../Images/user_dummy.png';
 import Button from '../../shared/components/FormElements/Button';
+import Input from '../../shared/components/FormElements/Input';
 import Modal from '../../shared/components/UIElements/Modal';
+import {
+	VALIDATOR_REQUIRE,
+	VALIDATOR_MINLENGTH,
+} from '../../shared/util/validators';
+import { useForm } from '../../shared/hooks/form-hook';
 const UserProfileInfo = (props) => {
 	const [creating, setCreating] = useState(false);
 
@@ -14,6 +20,36 @@ const UserProfileInfo = (props) => {
 	const cancelCreateEventHandler = () => {
 		setCreating(!creating);
 	};
+
+	const [formState, inputHandler] = useForm(
+		{
+			username: {
+				value: '',
+				isValid: true,
+			},
+			email: {
+				value: '',
+				isValid: true,
+			},
+			password: {
+				value: '',
+				isValid: true,
+			},
+			firstname: {
+				value: '',
+				isValid: true,
+			},
+			middlename: {
+				value: '',
+				isValid: true,
+			},
+			lastname: {
+				value: '',
+				isValid: true,
+			},
+		},
+		false
+	);
 
 	return (
 		<div className="c-user-profile">
@@ -62,7 +98,83 @@ const UserProfileInfo = (props) => {
 								</Button>
 							</React.Fragment>
 						}>
-						<h1>Modal</h1>
+						<div className="c-form">
+							<form
+								onSubmit={signupSubmitHandler}
+								className="c-form__body">
+								<Input
+									element="input"
+									id="username"
+									type="text"
+									placeholder="Enter your username here!"
+									label="Username:"
+									validators={[VALIDATOR_REQUIRE()]}
+									errorText="Please enter a valid username."
+									onInput={inputHandler}
+								/>
+
+								<Input
+									element="input"
+									id="email"
+									type="email"
+									placeholder="Enter your email here!"
+									label="Email:"
+									validators={[VALIDATOR_REQUIRE()]}
+									errorText="Please enter a valid email."
+									onInput={inputHandler}
+								/>
+
+								<Input
+									element="input"
+									id="password"
+									type="password"
+									placeholder="Enter your password here!"
+									label="Password:"
+									validators={[VALIDATOR_MINLENGTH(5)]}
+									errorText="Please enter a valid password with a minimum of 5 characters."
+									onInput={inputHandler}
+								/>
+
+								<Input
+									element="input"
+									id="firstname"
+									type="text"
+									placeholder="Enter your firstname here!"
+									label="First Name:"
+									validators={[VALIDATOR_REQUIRE()]}
+									errorText="Please enter a valid firstname."
+									onInput={inputHandler}
+								/>
+
+								<Input
+									element="input"
+									id="middlename"
+									type="text"
+									placeholder="Enter your middlename here!"
+									label="Middle Name:"
+									validators={[VALIDATOR_REQUIRE()]}
+									errorText="Please enter a valid middlename."
+									onInput={inputHandler}
+								/>
+
+								<Input
+									element="input"
+									id="lastname"
+									type="text"
+									placeholder="Enter your lastname here!"
+									label="Last Name:"
+									validators={[VALIDATOR_REQUIRE()]}
+									errorText="Please enter a valid lastname."
+									onInput={inputHandler}
+								/>
+
+								<div className="c-form-button">
+									<Button submit disabled={!formState.isValid}>
+										Sign Up
+									</Button>
+								</div>
+							</form>
+						</div>
 					</Modal>
 				)}
 			</div>
