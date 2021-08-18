aa6cbf0ed252febc4187941c91d304a18b1adc98
Mark <ratrateroo@gmail.com>
2021-08-18 22:07:34 +0800

    reverted the createportal function

--- frontend-graphql-blog/src/shared/components/UIElements/Modal.js
+++ frontend-graphql-blog/src/shared/components/UIElements/Modal.js
@@ -18,7 +18,7 @@ const ModalOverlay = (props) => {
 		</div>
 	);
 
-	return content;
+	return ReactDOM.createPortal(content, document.getElementById('modal-hook'));
 };
 
 const Modal = (props) => {
