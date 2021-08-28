a89d649f4fb6b8f03fefb4eb61f55a4c0b717590
Mark <ratrateroo@gmail.com>
2021-08-28 22:12:43 +0800

    added use form hook to the update profile form

--- frontend-graphql-blog/src/shared/components/UIElements/UpdateProfileForm.js
+++ frontend-graphql-blog/src/shared/components/UIElements/UpdateProfileForm.js
@@ -3,79 +3,112 @@ import React from 'react';
 import Input from '../FormElements/Input';
 
 import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from '../../util/validators';
+import { useForm } from '../../hooks/form-hook';
 
 import './UpdateProfileForm.css';
 
-const UpdateProfileForm = (props) => (
-	<div className="c-form">
-		<form onSubmit={props.onSubmit} className="c-form__body">
-			<Input
-				element="input"
-				id="username"
-				type="text"
-				placeholder="Enter your username here!"
-				label="Username:"
-				validators={[VALIDATOR_REQUIRE()]}
-				errorText="Please enter a valid username."
-				onInput={props.onInput}
-			/>
+const UpdateProfileForm = (props) => {
+	const [formState, inputHandler] = useForm(
+		{
+			username: {
+				value: '',
+				isValid: true,
+			},
+			email: {
+				value: '',
+				isValid: true,
+			},
+			password: {
+				value: '',
+				isValid: true,
+			},
+			firstname: {
+				value: '',
+				isValid: true,
+			},
+			middlename: {
+				value: '',
+				isValid: true,
+			},
+			lastname: {
+				value: '',
+				isValid: true,
+			},
+		},
+		false
+	);
 
-			<Input
-				element="input"
-				id="email"
-				type="email"
-				placeholder="Enter your email here!"
-				label="Email:"
-				validators={[VALIDATOR_REQUIRE()]}
-				errorText="Please enter a valid email."
-				onInput={props.onInput}
-			/>
+	return (
+		<div className="c-form">
+			<form onSubmit={props.onSubmit} className="c-form__body">
+				<Input
+					element="input"
+					id="username"
+					type="text"
+					placeholder="Enter your username here!"
+					label="Username:"
+					validators={[VALIDATOR_REQUIRE()]}
+					errorText="Please enter a valid username."
+					onInput={inputHandler}
+				/>
 
-			<Input
-				element="input"
-				id="password"
-				type="password"
-				placeholder="Enter your password here!"
-				label="Password:"
-				validators={[VALIDATOR_MINLENGTH(5)]}
-				errorText="Please enter a valid password with a minimum of 5 characters."
-				onInput={props.onInput}
-			/>
+				<Input
+					element="input"
+					id="email"
+					type="email"
+					placeholder="Enter your email here!"
+					label="Email:"
+					validators={[VALIDATOR_REQUIRE()]}
+					errorText="Please enter a valid email."
+					onInput={inputHandler}
+				/>
 
-			<Input
-				element="input"
-				id="firstname"
-				type="text"
-				placeholder="Enter your firstname here!"
-				label="First Name:"
-				validators={[VALIDATOR_REQUIRE()]}
-				errorText="Please enter a valid firstname."
-				onInput={props.onInput}
-			/>
+				<Input
+					element="input"
+					id="password"
+					type="password"
+					placeholder="Enter your password here!"
+					label="Password:"
+					validators={[VALIDATOR_MINLENGTH(5)]}
+					errorText="Please enter a valid password with a minimum of 5 characters."
+					onInput={inputHandler}
+				/>
 
-			<Input
-				element="input"
-				id="middlename"
-				type="text"
-				placeholder="Enter your middlename here!"
-				label="Middle Name:"
-				validators={[VALIDATOR_REQUIRE()]}
-				errorText="Please enter a valid middlename."
-				onInput={props.onInput}
-			/>
+				<Input
+					element="input"
+					id="firstname"
+					type="text"
+					placeholder="Enter your firstname here!"
+					label="First Name:"
+					validators={[VALIDATOR_REQUIRE()]}
+					errorText="Please enter a valid firstname."
+					onInput={inputHandler}
+				/>
 
-			<Input
-				element="input"
-				id="lastname"
-				type="text"
-				placeholder="Enter your lastname here!"
-				label="Last Name:"
-				validators={[VALIDATOR_REQUIRE()]}
-				errorText="Please enter a valid lastname."
-				onInput={props.onInput}
-			/>
-		</form>
-	</div>
-);
+				<Input
+					element="input"
+					id="middlename"
+					type="text"
+					placeholder="Enter your middlename here!"
+					label="Middle Name:"
+					validators={[VALIDATOR_REQUIRE()]}
+					errorText="Please enter a valid middlename."
+					onInput={inputHandler}
+				/>
+
+				<Input
+					element="input"
+					id="lastname"
+					type="text"
+					placeholder="Enter your lastname here!"
+					label="Last Name:"
+					validators={[VALIDATOR_REQUIRE()]}
+					errorText="Please enter a valid lastname."
+					onInput={inputHandler}
+				/>
+			</form>
+		</div>
+	);
+};
 
 export default UpdateProfileForm;
