651d2cb89363c08542d90669ac2397e334763641
Mark <ratrateroo@gmail.com>
2021-09-03 23:05:06 +0800

    added update profile modal

---
+++ frontend-graphql-blog/src/shared/components/UIElements/UpdatePictureModal.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import Button from '../FormElements/Button';
+
+
+
+import './UpdateModal.css';
+
+const modal = (props) => (
+	<div className={`c-modal`} style={props.style}>
+		<header className={`c-modal__header`}>
+			<h2 className={`c-modal__title`}>{props.title}</h2>
+		</header>
+		<div>
+			<div className={`c-modal__content-`}>
+				<div>
+				
+					<div>
+						<input type="file" onChange={} />
+						<button onClick={}>Upload!</button>
+					</div>
+					
+				</div>
+			</div>
+			<footer className={`c-modal__footer`}>
+				{props.canCancel && (
+					<div className="c-form-button">
+						<Button cancel onClick={props.onCancel}>
+							Cancel
+						</Button>
+					</div>
+				)}
+
+				{props.canConfirm && (
+					<div className="c-form-button">
+						<Button submit onClick={props.onConfirm}>
+							Ok
+						</Button>
+					</div>
+				)}
+			</footer>
+		</div>
+	</div>
+);
+
+export default modal;
