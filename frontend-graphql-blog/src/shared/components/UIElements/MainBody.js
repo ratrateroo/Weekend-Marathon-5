25c901611125189b841954016cfa8eca66292311
Mark <ratrateroo@gmail.com>
2021-09-05 22:45:13 +0800

    removed the state on the main body component

--- frontend-graphql-blog/src/shared/components/UIElements/MainBody.js
+++ frontend-graphql-blog/src/shared/components/UIElements/MainBody.js
@@ -5,21 +5,11 @@ import Button from '../FormElements/Button';
 import './MainBody.css';
 
 const MainBody = (props) => {
-	const [showModal, setShowModal] = useState(false);
-
-	const openModalHandler = () => {
-		setShowModal(true);
-	};
-
 	return (
 		<React.Fragment>
 			<main className="o-main-area">
 				<section className="o-section-container">
-					<div className="o-page-heading">
-						<Button heading onClick={openModalHandler}>
-							{props.title}
-						</Button>
-					</div>
+					<div className="o-page-heading">{props.title}</div>
 
 					<div className="o-page-body">{props.children}</div>
 				</section>
