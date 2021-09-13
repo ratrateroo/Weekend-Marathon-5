4f40e3c554990291a2f08f025c9093cd511c2ffa
Mark <ratrateroo@gmail.com>
2021-09-13 23:57:53 +0800

    added try to convert helper function

--- frontend-graphql-blog/src/testing/helpers/conversions.js
+++ frontend-graphql-blog/src/testing/helpers/conversions.js
@@ -5,3 +5,13 @@ export const toCelsius = (fahrenheit) => {
 export const toFahrenheit = (celsius) => {
 	return (celsius * 9) / 5 + 32;
 };
+
+export const tryConvert = (temperature, convert) => {
+	const input = parseFloat(temperature);
+	if (Number.isNaN(input)) {
+		return '';
+	}
+	const output = convert(input);
+	const rounded = Math.round(output * 1000) / 1000;
+	return rounded.toString();
+};
