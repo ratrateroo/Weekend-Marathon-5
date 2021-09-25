17abcc556777662e7120ea219af224d547064593
Mark <ratrateroo@gmail.com>
2021-09-25 23:16:22 +0800

    updated profile form and cleaned up some codes

--- frontend-graphql-blog/src/shared/components/UIElements/UpdateProfileForm.js
+++ frontend-graphql-blog/src/shared/components/UIElements/UpdateProfileForm.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import Input from '../FormElements/Input';
 
@@ -37,10 +37,6 @@ const UpdateProfileForm = (props) => {
 		},
 		false
 	);
-	// useEffect(() => {
-	// 	props.onChange(formState.isValid);
-	// 	console.log(formState);
-	// }, [formState]);
 
 	return (
 		<div className="c-form">
