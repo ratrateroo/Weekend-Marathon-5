bc3a5ea4f2542791e5487d6acae658c380c72cda
Mark <ratrateroo@gmail.com>
2021-09-27 22:30:19 +0800

    fixed some bugs in update profile info component

--- frontend-graphql-blog/src/user/components/UserProfileInfo.js
+++ frontend-graphql-blog/src/user/components/UserProfileInfo.js
@@ -23,23 +23,23 @@ const UserProfileInfo = (props) => {
 		setUpdating(!updating);
 	};
 
-	const updateProfileHandler = async (event) => {
-		event.preventDefault();
-		fetch('http://localhost:5000/users/signup', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({
-				username: formState.inputs.name.value,
-				email: formState.inputs.email.value,
-				password: formState.inputs.name.value,
-				firstname: formState.inputs.firstname.value,
-				middlename: formState.inputs.middlename.value,
-				lastname: formState.inputs.lastname.value,
-			}),
-		});
-	};
+	// const updateProfileHandler = async (event) => {
+	// 	event.preventDefault();
+	// 	fetch('http://localhost:5000/users/signup', {
+	// 		method: 'POST',
+	// 		headers: {
+	// 			'Content-Type': 'application/json',
+	// 		},
+	// 		body: JSON.stringify({
+	// 			username: formState.inputs.name.value,
+	// 			email: formState.inputs.email.value,
+	// 			password: formState.inputs.name.value,
+	// 			firstname: formState.inputs.firstname.value,
+	// 			middlename: formState.inputs.middlename.value,
+	// 			lastname: formState.inputs.lastname.value,
+	// 		}),
+	// 	});
+	// };
 
 	const updateProfileImageHandler = () => {
 		console.log('Update Profile Image');
@@ -87,8 +87,8 @@ const UserProfileInfo = (props) => {
 						header="Update Profile"
 						canCancel
 						canConfirm
-						onCancel={cancelCreateEventHandler}
-						onConfirm={updateProfileHandler}
+						// onCancel={cancelCreateEventHandler}
+						// onConfirm={updateProfileHandler}
 						// footer={
 						// 	<React.Fragment>
 						// 		<Button submit onClick={console.log('Clicked Update')}>
