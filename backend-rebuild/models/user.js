23e3ddf62c942dcab91491881a50f00ccccd7f0d
Mark <ratrateroo@gmail.com>
2021-10-12 23:38:13 +0800

    updated the user model for profile image

--- backend-rebuild/models/user.js
+++ backend-rebuild/models/user.js
@@ -28,6 +28,10 @@ const userSchema = new Schema(
 			type: String,
 			required: true,
 		},
+		profileimage: {
+			type: String,
+			required: false,
+		},
 		createdBlogs: [
 			{
 				type: Schema.Types.ObjectId,
